71ba3431c453a0ec43db656334dbedc67ab4da05
zamzam-shaaben <59848817+zamzam-shaaben@users.noreply.github.com>
2020-11-13 02:55:08 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import MyTaskList from "./component/My-Task-List";
+function App() {
+  return (
+      <div>
+        <MyTaskList />
+      </div>
+  );
+}
+
+export default App;
